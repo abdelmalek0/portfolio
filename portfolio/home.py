e56b66a6cf14a4eda4a851eb2d880ae582e686b8
abdelmalek0 <djamaa.abdelmalek98@gmail.com>
2024-09-27 12:27:33 +0100

    Added skills section

--- portfolio/home.py
+++ portfolio/home.py
@@ -1,26 +1,85 @@
 from fasthtml.common import H1, Div, Img, P, Span, A
 from components.navbar import Navbar
 
+AI_SKILLS = [
+    "Image classification",
+    "Object detection",
+    "Object tracking",
+    "Time series forecasting",
+    "Reinforcement learning",
+    "OCR",
+    "LLM",
+    "ChatLLM",
+    "RAG",
+]
+LANGUAGES = ["Python", "Java", "C"]
+LIBRARIES = [
+    "Tensorflow",
+    "OpenCV",
+    "Keras",
+    "Numpy",
+    "Pandas",
+    "Matplotlib",
+    "Pytorch",
+    "Scikit-learn",
+    "Flask",
+    "Langchain",
+]
+OTHERS = ["Git", "Github", "SQL", "SQlite", "Redis"]
+
 
 def Home():
     return Div(
         Navbar(index=0),
         Div(
-            Span("About", cls="text-lg font-medium"),
-            P("💼 I work as a data scientist @SmartPrints.", cls="text-gray-500"),
-            P(
-                "I've been developing Computer vision systems for a while.",
-                cls="text-gray-500",
+            Div(
+                Span("About", cls="text-lg font-medium"),
+                Div(
+                    P(
+                        "💼 I work as a data scientist @SmartPrints.",
+                        cls="text-gray-500",
+                    ),
+                    P(
+                        "I've been developing Computer vision systems for a while.",
+                        cls="text-gray-500",
+                    ),
+                    P(
+                        "I've been coding in 🐍 Python primarily for over 4 years.",
+                        cls="text-gray-500",
+                    ),
+                    P(
+                        "🌱 I’m currently working on RAG systems and learning Advanced concepts.",
+                        cls="text-gray-500",
+                    ),
+                    cls="flex flex-col gap-1",
+                ),
+                cls="flex flex-col gap-4",
             ),
-            P(
-                "I've been coding in 🐍 Python primarily for over 4 years.",
-                cls="text-gray-500",
+            Div(
+                Span("Skills", cls="text-lg font-medium"),
+                Span("AI", cls="font-medium text-gray-400"),
+                Div(
+                    *[Div(skill, cls="badge badge-primary") for skill in AI_SKILLS],
+                    cls="flex flex-row flex-wrap gap-2",
+                ),
+                Span("Languages", cls="font-medium text-gray-400"),
+                Div(
+                    *[Div(skill, cls="badge badge-success") for skill in LANGUAGES],
+                    cls="flex flex-row flex-wrap gap-2",
+                ),
+                Span("Libraries", cls="font-medium text-gray-400"),
+                Div(
+                    *[Div(skill, cls="badge badge-info") for skill in LIBRARIES],
+                    cls="flex flex-row flex-wrap gap-2",
+                ),
+                Span("Others", cls="font-medium text-gray-400"),
+                Div(
+                    *[Div(skill, cls="badge badge-secondary") for skill in OTHERS],
+                    cls="flex flex-row flex-wrap gap-2",
+                ),
+                cls="flex flex-col gap-4",
             ),
-            P(
-                "🌱 I’m currently working on RAG systems and learning Advanced concepts.",
-                cls="text-gray-500",
-            ),
-            cls="flex flex-col h-full w-full py-12 px-8 gap-2",
+            cls="flex flex-col h-full w-full py-12 px-8 gap-12",
         ),
         cls="flex flex-row mx-auto min-h-screen w-full max-w-5xl gap-20",
     )
